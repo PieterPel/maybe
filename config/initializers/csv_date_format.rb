e7bb828b70a86ed1ea9233188c3f3423fcd778d1
Pieter Pel <pelpieter@gmail.com>
2025-08-10 20:18:37 +0200

    Parser of new data formats

---
+++ config/initializers/csv_date_format.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+CSV::DateMatcher     = / \A(?: (\w+,?\s+)?\w+\s+\d{1,2},?\s+\d{2,4} |
+                            \d{4}-\d{2}-\d{2} |
+                            \d{8} )\z /x
+CSV::DateTimeMatcher =/ \A(?: (\w+,?\s+)?\w+\s+\d{1,2}\s+\d{1,2}:\d{1,2}:\d{1,2},?\s+\d{2,4} |
+            # ISO-8601 and RFC-3339 (space instead of T) recognized by (Date)Time.parse
+            \d{4}-\d{2}-\d{2}
+              (?:[T\s]\d{2}:\d{2}(?::\d{2}(?:\.\d+)?(?:[+-]\d{2}(?::\d{2})|Z)?)?)? |
+            \d{8}
+        )\z /x
